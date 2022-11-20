35abf771cb598d16c6526100bb055f76654fc62c
Abel Muriithi <abelmuriithi04@gmail.com>
2022-11-20 13:37:43 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name = "Ralph");
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name = "Bob");
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(name){
+    const copyOfCats= [...cats,name = "Broom"];
+    return copyOfCats;
+  }
+
+function prependCat(name){
+    const copyOfCats= [name = "Arnold", ...cats];
+    return copyOfCats;
+  }
+
+function removeLastCat(){
+    const copyOfCats= cats.slice(0, cats.length - 1);
+    return copyOfCats;
+  }
+
+  function removeFirstCat(){
+    const copyOfCats= cats.slice(1);
+    return copyOfCats;
+  }
